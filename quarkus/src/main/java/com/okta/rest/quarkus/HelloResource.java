835259e69390ceb0aa102e28d09ac1e0ce0a4e2c
Matt Raible <matt.raible@okta.com>
2021-06-17 14:17:11 -0600

    Switch from SmallRye JWT to OIDC

--- quarkus/src/main/java/com/okta/rest/quarkus/HelloResource.java
+++ quarkus/src/main/java/com/okta/rest/quarkus/HelloResource.java
@@ -1,24 +1,27 @@
 package com.okta.rest.quarkus;
 
+import io.quarkus.oidc.IdToken;
 import io.quarkus.security.Authenticated;
+import org.eclipse.microprofile.jwt.JsonWebToken;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.SecurityContext;
-import java.security.Principal;
 
 @Path("/hello")
 public class HelloResource {
+
+    @Inject
+    @IdToken
+    JsonWebToken idToken;
 
     @GET
     @Path("/")
     @Authenticated
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello(@Context SecurityContext context) {
-        Principal userPrincipal = context.getUserPrincipal();
-        return "Hello, " + userPrincipal.getName() + "!";
+    public String hello() {
+        return "Hello, " + this.idToken.getName() + "!";
     }
 }
