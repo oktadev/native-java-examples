d63b95c7ceff42348b3f27b39e6dbc4176e8fa1e
Matt Raible <matt.raible@okta.com>
2021-06-16 23:25:35 -0600

    Move HTTPS activation into code and use JWKS URI

--- spring-boot/src/main/java/com/okta/rest/DemoApplication.java
+++ spring-boot/src/main/java/com/okta/rest/DemoApplication.java
@@ -2,12 +2,14 @@ package com.okta.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.nativex.hint.NativeHint;
 
 @SpringBootApplication
+@NativeHint(options = "--enable-url-protocols=https")
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
 }
