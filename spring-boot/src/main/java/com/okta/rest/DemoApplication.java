70207ab0b644685b00fefc589a30daf3717cd4b3
Matt Raible <matt.raible@okta.com>
2021-09-13 16:26:10 -0600

    Upgrade to Spring Boot 2.5.4 and Spring Native 0.10.3

--- spring-boot/src/main/java/com/okta/rest/DemoApplication.java
+++ spring-boot/src/main/java/com/okta/rest/DemoApplication.java
@@ -2,20 +2,10 @@ package com.okta.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.nativex.hint.AccessBits;
 import org.springframework.nativex.hint.NativeHint;
-import org.springframework.nativex.hint.ResourceHint;
-import org.springframework.nativex.hint.TypeHint;
 
 @SpringBootApplication
-@NativeHint(options = "--enable-url-protocols=https")
-@ResourceHint(patterns = "com/okta/commons/configcheck/configuration-validator", isBundle = true)
-@TypeHint(typeNames = {
-    "com.okta.spring.boot.oauth.OktaOpaqueTokenIntrospectConditional",
-    "com.okta.spring.boot.oauth.OktaOpaqueTokenIntrospectConditional$ClientIdCondition",
-    "com.okta.spring.boot.oauth.OktaOpaqueTokenIntrospectConditional$ClientSecretCondition",
-    "com.okta.spring.boot.oauth.OktaOpaqueTokenIntrospectConditional$IntrospectionUriCondition"
-}, access = AccessBits.ALL)
+@NativeHint(options = "--enable-https")
 public class DemoApplication {
 
     public static void main(String[] args) {
