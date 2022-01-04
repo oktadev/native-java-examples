cc3afbefa5670c590c9c6e16265e6456ebbf97c4
Matt Raible <matt.raible@okta.com>
2022-01-04 10:37:05 -0700

    Upgrade to Helidon 2.4.1

--- helidon/src/main/java/com/okta/rest/HelloApplication.java
+++ helidon/src/main/java/com/okta/rest/HelloApplication.java
@@ -10,6 +10,7 @@ import java.util.Set;
 @LoginConfig(authMethod = "MP-JWT")
 @ApplicationScoped
 public class HelloApplication extends Application {
+
     @Override
     public Set<Class<?>> getClasses() {
         return Set.of(HelloResource.class);
--- helidon/src/main/java/com/okta/rest/package-info.java
+++ helidon/src/main/java/com/okta/rest/package-info.java
@@ -1,1 +1,2 @@
+
 package com.okta.rest;
